8898fb78c61f1dd8d24cb6aca4914b7e101ac460
Buck DeFore <bdefore@gmail.com>
2015-01-22 15:04:47 -0800

    adding dist and src

---
+++ dist/angular-cache-proxy.js
@@ -0,0 +1,194 @@
+var module;
+
+module = angular.module('angular-cache-proxy', ['LocalStorageModule', 'restangular']);
+
+module.provider('CacheProxy', function() {
+  var configurables;
+  configurables = {
+    verifyCache: true,
+    baseUrl: "",
+    unimportantKeys: [],
+    expiry: 1000 * 60 * 60 * 24 * 7,
+    cacheInvalidCallback: null
+  };
+  this.configure = function(overrides) {
+    return angular.extend(configurables, overrides);
+  };
+  this.$get = function($q, $window, $timeout, $http, Restangular, localStorageService) {
+    var CacheProxy;
+    return CacheProxy = (function() {
+      var getCache, getCacheAndRequest, makeRequest, pruneUnimportantData, setCache, validate;
+
+      function CacheProxy() {}
+
+      getCache = function(uniqueIdentifier) {
+        var cacheObj;
+        cacheObj = localStorageService.get(uniqueIdentifier);
+        if (cacheObj == null) {
+          return void 0;
+        }
+        if (Date.now() > cacheObj.timestamp + configurables.expiry) {
+          console.log("Cache for " + uniqueIdentifier + " older than expiry, ignoring");
+          return void 0;
+        }
+        return cacheObj.json;
+      };
+
+      setCache = function(key, value) {
+        var cacheObj;
+        cacheObj = {
+          json: value,
+          timestamp: Date.now()
+        };
+        return localStorageService.set(key, cacheObj);
+      };
+
+      validate = function(oldVersion, newVersion) {
+        var version, _i, _len, _ref;
+        if (newVersion.updatedAt != null) {
+          return oldVersion.updatedAt === newVersion.updatedAt;
+        } else {
+          _ref = [newVersion, oldVersion];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            version = _ref[_i];
+            pruneUnimportantData(version);
+          }
+          return JSON.stringify(oldVersion) === JSON.stringify(newVersion);
+        }
+      };
+
+      pruneUnimportantData = function(obj) {
+        var data, key, keyToMatch, _i, _len, _ref, _results;
+        _results = [];
+        for (key in obj) {
+          data = obj[key];
+          _ref = configurables.unimportantKeys;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            keyToMatch = _ref[_i];
+            delete obj[keyToMatch];
+          }
+          if (typeof data === "object") {
+            _results.push(pruneUnimportantData(data));
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      };
+
+      getCacheAndRequest = function(uniqueIdentifier, requestFn) {
+        return $q((function(_this) {
+          return function(resolve, reject) {
+            var cachedResponse;
+            cachedResponse = getCache(uniqueIdentifier);
+            $timeout(function() {
+              if (cachedResponse != null) {
+                return resolve(cachedResponse);
+              }
+            }, 0);
+            if (configurables.verifyCache || (cachedResponse == null)) {
+              return makeRequest(uniqueIdentifier, requestFn).then(function(results) {
+                return resolve(results);
+              });
+            }
+          };
+        })(this));
+      };
+
+      makeRequest = function(uniqueIdentifier, requestFn) {
+        return $q((function(_this) {
+          return function(resolve, reject) {
+            return $timeout(function() {
+              return requestFn().then(function(response) {
+                var cached;
+                response = Restangular.stripRestangular(response);
+                if (response.data != null) {
+                  response = response.data;
+                }
+                cached = getCache(uniqueIdentifier);
+                setCache(uniqueIdentifier, response);
+                if (cached == null) {
+                  return resolve(response);
+                } else if (!validate(cached, response) && (configurables.cacheInvalidCallback != null)) {
+                  return configurables.cacheInvalidCallback(response);
+                }
+              });
+            }, 0);
+          };
+        })(this));
+      };
+
+      CacheProxy.paramsObjectToString = function(params) {
+        var key, output, value;
+        output = "";
+        for (key in params) {
+          value = params[key];
+          output += "&" + key + "=" + value;
+        }
+        return "?" + output.substr(1);
+      };
+
+      CacheProxy.one = function(model, id, params) {
+        var requestFn, uniqueIdentifier;
+        uniqueIdentifier = "" + configurables.baseUrl + "/" + model + "/" + id;
+        if (params != null) {
+          uniqueIdentifier += this.paramsObjectToString(params);
+        }
+        requestFn = (function() {
+          return Restangular.one(model, id).get(params);
+        });
+        return getCacheAndRequest(uniqueIdentifier, requestFn);
+      };
+
+      CacheProxy.list = function(model, id, params) {
+        var requestFn, uniqueIdentifier;
+        if (typeof params === "object") {
+          params = this.paramsObjectToString(params);
+        }
+        uniqueIdentifier = "" + configurables.baseUrl + "/" + model;
+        if (id != null) {
+          uniqueIdentifier += "/" + id;
+        }
+        if (params != null) {
+          uniqueIdentifier += "/" + params;
+        }
+        requestFn = (function() {
+          return Restangular.one(model, id).getList(params);
+        });
+        return getCacheAndRequest(uniqueIdentifier, requestFn);
+      };
+
+      CacheProxy.get = function(url, params) {
+        var requestFn, uniqueIdentifier;
+        if (typeof params === "object") {
+          params = this.paramsObjectToString(params);
+        }
+        if (params != null) {
+          url += params;
+        }
+        if (configurables.baseUrl != null) {
+          url = "" + configurables.baseUrl + "/" + url;
+        }
+        uniqueIdentifier = url;
+        requestFn = (function() {
+          return $http.get(url);
+        });
+        return getCacheAndRequest(uniqueIdentifier, requestFn);
+      };
+
+      CacheProxy.store = function(id, obj) {
+        return setCache("" + configurables.baseUrl + "/" + id, obj);
+      };
+
+      CacheProxy.onCacheInvalid = function(callback) {
+        return configurables.cacheInvalidCallback = callback;
+      };
+
+      new CacheProxy();
+
+      return CacheProxy;
+
+    })();
+  };
+  return null;
+});
---
+++ src/angular-cache-proxy.coffee
@@ -0,0 +1,143 @@
+# Angular Cache Proxy for AngularJS - Store and retrieve API responses in local storage
+# @version v0.0.1 - 2015-01-21
+# @link http://github.com/learnist/angular-cache-proxy
+# @author Buck DeFore <julien@revolunet.com>
+# @license MIT License, http://www.opensource.org/licenses/MIT
+
+module = angular.module 'angular-cache-proxy', [
+  'LocalStorageModule',
+  'restangular'
+]
+
+module.provider 'CacheProxy', ->
+
+  configurables =
+
+    # With verifyCache set to false, if data at uniqueIdentifier exists in local storage
+    # then we do not call the API for the latest version to verify that the local is up to date
+    verifyCache: true
+
+    # Prepending to all $http calls. If using Restangular, use
+    # RestangularProvider.setBaseUrl(<baseUrl>) in your app.config
+    baseUrl: ""
+
+    # Keys which either change too frequently or are not important enough to trigger
+    # a reload of the page should they not match values in cache
+    unimportantKeys: []
+
+    # When to consider a cached version stale and re-request from server, in milliseconds
+    expiry: 1000 * 60 * 60 * 24 * 7 # one week
+
+    # A callback to fire every time cache does not match the latest from the API
+    cacheInvalidCallback: null
+
+  @configure = (overrides) ->
+    angular.extend configurables, overrides
+
+  @$get = ($q, $window, $timeout, $http, Restangular, localStorageService) ->
+
+    class CacheProxy
+
+      getCache = (uniqueIdentifier) ->
+        cacheObj = localStorageService.get uniqueIdentifier
+        return undefined if !cacheObj?
+        if Date.now() > cacheObj.timestamp + configurables.expiry
+          console.log "Cache for #{uniqueIdentifier} older than expiry, ignoring"
+          return undefined
+        cacheObj.json
+
+      setCache = (key, value) ->
+        cacheObj =
+          json: value
+          timestamp: Date.now()
+        localStorageService.set key, cacheObj
+
+      validate = (oldVersion, newVersion) ->
+        if newVersion.updatedAt?
+          # such as board data, we don't care if views have incremented
+          oldVersion.updatedAt == newVersion.updatedAt
+        else
+          # such as /categories/0?extraFields=promoted
+          pruneUnimportantData version for version in [newVersion, oldVersion]
+
+          JSON.stringify(oldVersion) == JSON.stringify(newVersion)
+
+      pruneUnimportantData = (obj) ->
+        for key, data of obj
+          for keyToMatch in configurables.unimportantKeys
+            delete obj[keyToMatch]
+          pruneUnimportantData(data) if typeof(data) is "object"
+
+      getCacheAndRequest = (uniqueIdentifier, requestFn) ->
+        $q (resolve, reject) =>
+          cachedResponse = getCache(uniqueIdentifier)
+
+          # Resolve promise with cached version if there, but on next tick to simulate asynchronous behavior
+          $timeout =>
+            resolve(cachedResponse) if cachedResponse?
+          , 0
+
+          # Then fetch from backend and notify if mismatch
+          if configurables.verifyCache or !cachedResponse?
+            makeRequest(uniqueIdentifier, requestFn).then (results) ->
+              resolve results
+
+      makeRequest = (uniqueIdentifier, requestFn) ->
+        $q (resolve, reject) =>
+          # timeout because ui router will withhold full resolution if http requests are outgoing (why?!)
+          $timeout =>
+            requestFn().then (response) =>
+
+              response = Restangular.stripRestangular response
+
+              # $http would return a response object, convenience check here to
+              # unify with returning the data (a la Restangular)
+              response = response.data if response.data?
+
+              cached = getCache(uniqueIdentifier)
+              setCache uniqueIdentifier, response
+
+              if !cached?
+                resolve response
+              else if !validate(cached, response) && configurables.cacheInvalidCallback?
+                configurables.cacheInvalidCallback(response)
+          , 0
+
+      @paramsObjectToString: (params) ->
+        output = ""
+        output += "&#{key}=#{value}" for key, value of params
+        "?" + output.substr(1)
+
+      @one: (model, id, params) ->
+        uniqueIdentifier = "#{configurables.baseUrl}/#{model}/#{id}"
+        uniqueIdentifier += @paramsObjectToString(params) if params?
+        requestFn = (-> Restangular.one(model, id).get(params))
+        getCacheAndRequest(uniqueIdentifier, requestFn)
+
+      @list: (model, id, params) ->
+        if typeof params is "object"
+          params = @paramsObjectToString params
+        uniqueIdentifier = "#{configurables.baseUrl}/#{model}"
+        uniqueIdentifier += "/#{id}" if id?
+        uniqueIdentifier += "/#{params}" if params?
+        requestFn = (-> Restangular.one(model, id).getList(params))
+        getCacheAndRequest(uniqueIdentifier, requestFn)
+
+      @get: (url, params) ->
+        if typeof params is "object"
+          params = @paramsObjectToString params
+        url += params if params?
+        url = "#{configurables.baseUrl}/#{url}" if configurables.baseUrl?
+        uniqueIdentifier = url
+        requestFn = (-> $http.get(url))
+        getCacheAndRequest(uniqueIdentifier, requestFn)
+
+      @store: (id, obj) ->
+        setCache "#{configurables.baseUrl}/#{id}", obj
+
+      @onCacheInvalid: (callback) ->
+        configurables.cacheInvalidCallback = callback
+
+      new CacheProxy()
+
+  null
